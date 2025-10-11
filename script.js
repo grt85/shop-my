17f51c5e4a2308341af26c50698d880eb0340af7
grt85 <fffvladimir952@gmail.com>
2025-10-11 05:27:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -229,7 +229,7 @@ phoneInput.dispatchEvent(event);
 
   saveCart();
 
-  fetch('https://uneloping-jeanna-erythrismal.ngrok-free.dev/api/order', {
+  fetch('https://shop-my-86on.onrender.com'/api/order', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(cart)
@@ -457,6 +457,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("contacts").style.display = "none";
     document.getElementById("overlay").style.display = "none";
   }
+
 
 
 
