59e1a1e94a2731fd494c8ef263a8ca22bb0366e2
grt85 <fffvladimir952@gmail.com>
2025-11-27 19:40:51 +0200

    Refactor server.js for improved structure and clarity

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const ADMIN_TOKEN = process.env.ADMIN_TOKEN;
 
 // ===== Middleware =====
 app.use(cors({
-  origin: ['http://127.0.0.1:5500', "https://shop-my-86on.onrender.com", 'http://localhost:3000'],
+  origin: ['http://127.0.0.1:5500', "https://shop-my-86on.onrender.com/api/orders", 'http://localhost:3000'],
   methods: ['GET','POST','PUT','DELETE'],
   allowedHeaders: ['Content-Type','Authorization']
 }));
@@ -237,3 +237,4 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`🚀 Сервер запущено на http://localhost:${PORT}`);
 });
+
